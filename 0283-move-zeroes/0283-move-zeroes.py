642fe4e13b72c523436529ed2f1129540675523c
GAURAV NEGI <gauravnegigvps@gmail.com>
2026-08-07 00:01:43 +0530

    3 ms (81.62%) 20.5 MB (24.26%) Array 0283-move-zeroes .py Easy

---
+++ 0283-move-zeroes/0283-move-zeroes.py
@@ -0,0 +1,14 @@
+class Solution:
+    def moveZeroes(self, nums: List[int]) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        insert_position = 0 
+
+        for i in range(len(nums)):
+            if nums[i]!= 0:
+                if i != insert_position :
+
+                  nums[insert_position], nums[i]= nums[i], nums[insert_position]
+
+                insert_position += 1
