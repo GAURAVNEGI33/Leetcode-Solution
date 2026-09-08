2b105fd7602c05016403bc9e3240e40937c2b76b
GAURAV NEGI <gauravnegigvps@gmail.com>
2026-09-09 00:11:20 +0530

    27 ms (34.3%) 19.7 MB (8.93%) Hash Table 0383-ransom-note .py Easy

--- 0383-ransom-note/0383-ransom-note.py
+++ 0383-ransom-note/0383-ransom-note.py
@@ -1,11 +1,19 @@
 class Solution:
     def canConstruct(self, ransomNote: str, magazine: str) -> bool:
-        fre ={}
-        for char in  magazine :
-            fre[char]= fre.get(char,0)+1
-        for char in ransomNote:
-            if char not in fre or fre[char] == 0:
-                return False
-            fre[char]-=1
-        return True
+        if len(ransomNote) > len(magazine):
+            return False
 
+        frequency = [0] * 26
+
+        for char in magazine:
+            index = ord(char) - ord("a")
+            frequency[index] += 1
+
+        for char in ransomNote:
+            index = ord(char) - ord("a")
+            frequency[index] -= 1
+
+            if frequency[index] < 0:
+                return False
+
+        return True
