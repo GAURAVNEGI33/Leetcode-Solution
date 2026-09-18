61f82fa2ff10f76ceaa2008b919aabea9d084ea9
GAURAV NEGI <gauravnegigvps@gmail.com>
2026-09-18 22:59:54 +0530

    17 ms (48.12%) 32.3 MB (55.13%) Array 0217-contains-duplicate .py Easy

--- 0217-contains-duplicate/0217-contains-duplicate.py
+++ 0217-contains-duplicate/0217-contains-duplicate.py
@@ -1,13 +1,17 @@
 class Solution:
     def containsDuplicate(self, nums: List[int]) -> bool:
         seen = set()
-         
+
+
         for num in nums:
             if num in seen:
                 return True
-            else:
-                
-                
-                seen.add(num)
+
+            seen.add(num)
+
         return False
+
+
+         
+       
             
